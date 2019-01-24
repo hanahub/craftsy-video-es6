e346bec746a3f37d0f4f882bd9c4287b4fd929ea
SocialRoot <dev@socialroot.co>
2019-01-24 09:55:03 -0800

    initial commit

--- HTML5PausePatch.js
+++
@@ -1,8 +0,0 @@
-// This patch ensures Safari pauses, see BLU-408
-export class HTML5PausePatch {
-  constructor (el) {
-    el.addEventListener('pause', function () {
-      this.pause()
-    })
-  }
-}
